cad18d538d2bce1f8b9afd50e3b47f5ac7f38fbf
lin564 <83978418+lin564@users.noreply.github.com>
2023-04-11 16:03:47 -0700

    Update

---
+++ apiKey.js
@@ -0,0 +1,4 @@
+ /* Copy this into a file named apiKey.js */
+const apiKey = '1bDKDKL3mEx50nn4pFy6d86zPNpgqMKsIJC8Fd1xj';
+const appId = 'com.ultisim.linda.bernard.microverse';
+export default  {apiKey, appId};
