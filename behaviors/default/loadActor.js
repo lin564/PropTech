263006d8efaf7fea7b5a4143b1b574a09d081d32
lin564 <83978418+lin564@users.noreply.github.com>
2023-05-02 17:44:40 -0400

    clip index update to 1

--- behaviors/default/loadActor.js
+++ behaviors/default/loadActor.js
@@ -7,7 +7,7 @@ class LoadActor {
         const cards = 
         [
             {
-                animationClipIndex: 0,
+                animationClipIndex: 1,
                 animationStartTime: 26751811,
                 dataLocation: "3VSLcwcr1Dbl27-xQGK3bRfgzIrtIxVuSjaMjktnZHS4PiIiJiVseXkwPzozJXgjJXg1JDknIzMieD85eSN5OD8ZJB0QYGNkPhgRAzgFZCY4Ih1gEgcEZB8nZHk1OTt4IzoiPyU_O3g6PzgyN3g0MyQ4NyQyeDs_NSQ5IDMkJTN5PWMeNARiLwwZOxkQZyARGwMsIg4xHGcHBgViJjszDDEDbjdiFGcQZCEfG3kyNyI3eRsVFGMkMglhYz8fZgc3ARgXOQAEDGYQJDglLx9gJx8BFRggOhduZDQGexs",
                 dataScale: [1,1,1],
