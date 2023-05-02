d94f408447cdf2d0f7560005aa3246c3348f0766
lin564 <83978418+lin564@users.noreply.github.com>
2023-05-02 16:32:14 -0400

    scale change

--- behaviors/default/loadActor.js
+++ behaviors/default/loadActor.js
@@ -10,7 +10,7 @@ class LoadActor {
                 animationClipIndex: 0,
                 animationStartTime: 26751811,
                 dataLocation: "3VSLcwcr1Dbl27-xQGK3bRfgzIrtIxVuSjaMjktnZHS4PiIiJiVseXkwPzozJXgjJXg1JDknIzMieD85eSN5OD8ZJB0QYGNkPhgRAzgFZCY4Ih1gEgcEZB8nZHk1OTt4IzoiPyU_O3g6PzgyN3g0MyQ4NyQyeDs_NSQ5IDMkJTN5PWMeNARiLwwZOxkQZyARGwMsIg4xHGcHBgViJjszDDEDbjdiFGcQZCEfG3kyNyI3eRsVFGMkMglhYz8fZgc3ARgXOQAEDGYQJDglLx9gJx8BFRggOhduZDQGexs",
-                dataScale: [10, 10, 10],
+                dataScale: [2, 2, 2],
                 fileName: "/Green Guy.glb",
                 layers: ["pointer"],
                 modelType: "glb",
